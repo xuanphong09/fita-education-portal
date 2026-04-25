aac207eb7a3f1baf46242e98b2d6e765f97566e8
xphongmaru <671762@sv.vnua.edu.vn>
2026-04-25 14:36:10 +0700

    fix file subject

--- config/livewire.php
+++ config/livewire.php
@@ -3,8 +3,12 @@
 return [
     'payload' => [
         // Increase limit for rich-text post content (TinyMCE with tables/images).
-        'max_size' => 16 * 1024 * 1024, // 4MB
+        'max_size' => 30 * 1024 * 1024, // 4MB
         'max_components' => 50,
+    ],
+    'temporary_file_upload' => [
+        'disk' => null,
+        'rules' => ['required', 'file', 'max:30720'],
     ],
 ];
 
